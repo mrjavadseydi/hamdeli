85c11c184c4ff0eb58a85d64ac37da5a95dea2db
mohammad javad seydi <seydi.birjand@gmail.com>
2020-12-02 11:38:58 +0330

    dynomic wellcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta class="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <title>همدلی</title>
+    <title>{{getOption('title')}}</title>
     <!-- Don't forget to add your metadata here -->
     <link rel='stylesheet' href='{{asset('css/app.css')}}'/>
     <link rel='stylesheet' href='{{asset('css/style.css')}}'/>
@@ -47,15 +47,8 @@
         <div class="container">
             <div class="hero__content">
                 <div class="hero__content__inner" id='navConverter'>
-                    <h1 class="hero__title" style="text-align: center">آغاز یک همدلی </h1>
-                    <p class="hero__text" style="text-align: right">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از
-                        صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و
-                        سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود
-                        ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و
-                        متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان
-                        خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری
-                        موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای
-                        اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+                    <h1 class="hero__title" style="text-align: center">{{getOption('title')}} </h1>
+                    <p class="hero__text" style="text-align: right">{{getOption('sitedes')}}</p>
                     <button type="button" class="button button__accent" data-toggle="modal" data-target="#exampleModal"
                     >ثبت نام نیازمندان
                     </button>
@@ -71,8 +64,8 @@
 <!-- Steps -->
 <div class="steps landing__section">
     <div class="container">
-        <h2>نحوه کار ما به چه صورت است؟</h2>
-        <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</p>
+        <h2>{{getOption('work1')}}</h2>
+        <p>{{getOption('work2')}}</p>
     </div>
     <div class="container">
         <div class="steps__inner text-center">
@@ -80,28 +73,22 @@
                 <div class="step__media ">
                     <img src="{{asset('images/5893.png')}}" class="step__image m-auto  ">
                 </div>
-                <h4 class=" text-center">لورم ایپسوم</h4>
-                <p class="step__text  text-center">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-                    استفاده از طراحان
-                    گرافیک است</p>
+                <h4 class=" text-center">{{getOption('title1')}}</h4>
+                <p class="step__text  text-center">{{getOption('des1')}}</p>
             </div>
             <div class="step text-cente">
                 <div class="step__media">
                     <img src="{{asset('images/6144.png')}}" class="step__image m-auto">
                 </div>
-                <h4>لورم ایپسوم</h4>
-                <p class="step__text  text-cente">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                    از طراحان
-                    گرافیک است</p>
+                <h4>{{getOption('title2')}}</h4>
+                <p class="step__text  text-cente">{{getOption('des2')}}</p>
             </div>
             <div class="step text-cente">
                 <div class="step__media ">
                     <img src="{{asset('images/6154.png')}}" class="step__image m-auto">
                 </div>
-                <h4>لورم ایپسوم</h4>
-                <p class="step__text  text-cente">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                    از طراحان
-                    گرافیک است</p>
+                <h4>{{getOption('title3')}}</h4>
+                <p class="step__text  text-cente">{{getOption('des3')}}</p>
             </div>
         </div>
     </div>
