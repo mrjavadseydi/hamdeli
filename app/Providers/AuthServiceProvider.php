2bd7b330f15af02d3994beadb766b7c038eec21f
mohammad javad seydi <seydi.birjand@gmail.com>
2021-02-20 09:38:49 +0330

    init gates

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Permission;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -22,7 +23,14 @@ class AuthServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
+{
+
+        foreach (Permission::all() as $permission) {
+
+            Gate::define($permission->name , function($user) use ($permission){
+                return $user->hasPermission($permission);
+            });
+        }
         $this->registerPolicies();
 
         //
