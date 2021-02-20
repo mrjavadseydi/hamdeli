6a9b023bd2d0ac5108691bf02c06cef69db77426
mohammad javad seydi <seydi.birjand@gmail.com>
2021-02-20 09:38:37 +0330

    gurad new models

--- app/Models/NeederPlanHelp.php
+++ app/Models/NeederPlanHelp.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class NeederPlanHelp extends Model
 {
     use HasFactory;
+    protected $guarded =[];
 }
--- app/Models/NeedyRequest.php
+++ app/Models/NeedyRequest.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class NeedyRequest extends Model
 {
     use HasFactory;
+    protected $guarded =[];
 }
