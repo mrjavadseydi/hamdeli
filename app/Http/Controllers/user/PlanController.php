fd43cfcfc7ca80ecb4ef017d1d4b94a030d24e76
mrJavad <miniopera165@gmail.com>
2021-02-24 13:17:35 +0330

    show user Plan

--- app/Http/Controllers/user/PlanController.php
+++ app/Http/Controllers/user/PlanController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
+use App\Models\Group;
+use App\Models\Plan;
+use App\Models\UserGroup;
 use Illuminate\Http\Request;
 
 class PlanController extends Controller
 {
     public function index(){
-
+        $gro = Group::whereIn('id',UserGroup::where('user_id',auth()->id())->pluck('group_id'));
+        $group = $gro->get();
+        return view('admin.userplan.index',compact('group'));
     }
 }
